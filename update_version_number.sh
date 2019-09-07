06f5193d484c2f5af1e1dc5d1eec396f78e979e0
Dr. Setz <thomas@setz.de>
2019-09-08 01:22:47 +0200

    fix typos

--- update_version_number.sh
+++ update_version_number.sh
@@ -5,6 +5,7 @@ set -e
 
 git log -1 --oneline >> CHANGES.txt
 source ./venv/bin/activate && python version_incr.py
-git commit -m"autocommit from ci"  CHANGES.txt VERSION.txt
+VERSION=`cat VERSION.txt`
+git commit -m"autocommit from ci $VERSION"  CHANGES.txt VERSION.txt
 git push
 
