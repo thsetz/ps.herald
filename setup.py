3a16b2ca16af76640b116d9547a5fd8262dca75f
Dr. Setz <thomas@setz.de>
2019-09-07 21:41:48 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,14 @@
-from setuptools import setup, find_packages, find_namespace_packages
+from setuptools import setup, find_packages
 import os
 import sys
 import subprocess
 
-
-def _read(name):
-	    def appendnl(s):
-	        if not s or s[-1] != '\n':
-	            return s + '\n'
-	        else:
-	            return s
-	
-	    with open(name) as f:
-	        return appendnl(f.read())
-
-#if False: pass
-#elif  "install" or "develop" in sys.argv:
-#    result = subprocess.call("pip install -r requirements/common.txt ", shell=True)
-#    if result != 0:
-#        sys.stdout.write("Not quite shure what to do here")
-    	
-
-#open("requirements.tyt").read().decode('utf-8').split()
 install_requires= open("requirements.txt").read().split()  
-
-VERSION = _read('VERSION.txt').strip()
+VERSION = open('VERSION.txt').read().strip()
 
 here = os.path.abspath(os.path.dirname(__file__))
 open(os.path.join(here,'src', 'ps_herald','herald_package_version.py'), 'w').write('version = "%s"' % VERSION)
 
-print(find_packages('src'))
-print(find_namespace_packages(where='src'))
 
 setup(name='ps_herald',
       version=VERSION,
@@ -39,18 +17,23 @@ setup(name='ps_herald',
                        open(os.path.join( "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          #'Development Status :: 5 - Stable',
+          'Environment :: Console',
+          #'Intended Audience :: DevOps',
+          'License :: Other/Proprietary License',
+          'Operating System :: POSIX :: Linux',
+          #'Programming Language :: Python :: 3.6 :: ',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Utilities'
+      ],
+
       keywords='PS',
       author='Setz',
-      url='www.setz.de',
-      author_email='Thomas.Setz@haufe-lexware.com',
+      url='https://bitbucket.org/drsetz/ps.herald',
+      author_email='Thomas.Setz@acm.org',
       license='', # need/want 'ZPL'?
       packages=find_packages('src'),
       package_dir={'':'src'},
-      #namespace_packages=['ps' ],
-      #namespace_packages=find_namespace_packages(where='src'),
       include_package_data=True,
       dependency_links=["https://vl-pypi.haufe-ep.de/ps/DEVELOPMENT",],
       zip_safe=False,
