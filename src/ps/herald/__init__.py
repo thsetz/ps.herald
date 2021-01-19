0bc52f437e88fa593c4156c6bbfa6ae0b9f6bd49
Dr. Setz <thomas@setz.de>
2021-01-19 05:58:09 +0100

    intermediate ci

---
+++ src/ps/herald/__init__.py
@@ -0,0 +1,8 @@
+import pkg_resources
+# get the version number defined in setup.py
+__version__ = pkg_resources.get_distribution("ps.herald").version
+
+
+
+
+
---
+++ src/ps/herald/util.py
@@ -0,0 +1,5 @@
+"""
+>>> import os
+>>> assert True
+"""
+
