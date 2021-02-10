5856de6aded368a8842e884cf13c14a20584c8fe
Dr. Setz <thomas@setz.de>
2021-02-10 21:22:15 +0100

    components cmessages clog_form clog_list and services smessage slog_data created

--- ps-herald-angular/src/app/app.module.ts
+++ ps-herald-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CmessagesComponent } from './cmessages/cmessages.component';
+import { ClogFormComponent } from './clog-form/clog-form.component';
+import { ClogListComponent } from './clog-list/clog-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CmessagesComponent,
+    ClogFormComponent,
+    ClogListComponent
   ],
   imports: [
     BrowserModule,
